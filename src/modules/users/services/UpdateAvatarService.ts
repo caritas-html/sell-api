7e27fd22dce16b2ea5e4593b712f4f0f0515a4fa
Leonardo Lima <leoexori@hotmail.com>
2022-11-27 19:40:03 -0300

    feat: add upload image lib and config files

---
+++ src/modules/users/services/UpdateAvatarService.ts
@@ -0,0 +1,34 @@
+import AppError from "@shared/errors/AppError";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+import path from "path";
+import fs from "fs";
+import uploadConfig from "@config/upload";
+
+// if the user is authenticated, it will be used
+interface IRequest {
+  userId: string;
+  avatarFilename: string;
+}
+
+class UpdateAvatarService {
+  public async execute({ userId, avatarFilename }: IRequest) {
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(userId);
+
+    if (!user) throw new AppError("User not found!");
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarExists = await fs.promises.stat(userAvatarFilePath); // return a promise with the status code of the path
+
+      if (userAvatarExists) await fs.promises.unlink(userAvatarFilePath);
+    }
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateAvatarService;
